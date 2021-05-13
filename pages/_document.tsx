5d24133bf12d1e3d3673cd634761c9e29a4d2cea
swarn <swarnchahal@Swarns-MacBook-Pro.local>
2021-05-13 16:28:48 -0400

    minor changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,7 @@ import Document, {
   Html, Head, Main, NextScript,
 } from 'next/document';
 import config from '../../chewy.json';
+import Header from "../src/common/components/Header/Header";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -17,7 +18,16 @@ class MyDocument extends Document {
           <Head>
             <script type="application/javascript" src="http://localhost:9001/auth/js/keycloak.js" />
           </Head>
-        ) : null}
+        ) : (
+          <Head>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+    posthog.init('3flHxByUL7CLtlnaUpXOrjQ4V5484kHJdT2MuHZgASg',{api_host:'http://0.0.0.0:8000'})`,
+              }}
+            />
+          </Head>
+        )}
         <body>
           <Main />
           <NextScript />
--- src/common/components/layout/PrimaryLayout/PrimaryLayout.tsx
+++ src/common/components/layout/PrimaryLayout/PrimaryLayout.tsx
@@ -1,10 +1,8 @@
 import { memo } from "react";
 import {
-  Grid, makeStyles, Typography,
+  Button,
+  Grid, makeStyles,
 } from "@material-ui/core";
-import Login from "../../../../modules/auth/components/Login";
-import { useAppSelector } from "../../../../store";
-import Navbar from "../../Toolbar/Navbar";
 import config from "../../../../../../chewy.json";
 
 export interface PrimaryLayoutProps {
@@ -17,37 +15,20 @@ const useStyles = makeStyles(() => ({
     minHeight: '100%',
   },
 }));
-const PrimaryLayout = memo(({ Component, pageProps }: PrimaryLayoutProps) => {
+const PrimaryLayout = memo(() => {
   const classes = useStyles();
-  const isAuthed = useAppSelector(((state) => !!state.auth.token));
-  const clientAuthed = useAppSelector(((state) => !!state.auth.clientAuthed));
   console.log(config);
   const isKeycloakEnabled = config.modules.auth.enabled;
-    console.log(isKeycloakEnabled);
-  if (!isKeycloakEnabled) {
-    return (
-      <Grid container justify="center" alignItems="center" className={classes.loginPage}>
-        <Grid item>
-          <h1>keycloak is not enabled</h1>
-        </Grid>
+  console.log(isKeycloakEnabled);
+
+  return (
+    <Grid container justify="center" alignItems="center" className={classes.loginPage}>
+      <Grid item>
+        <h1>keycloak is not enabled</h1>
+        <Button>Submit</Button>
       </Grid>
-    );
-  }
-  if (!isAuthed) {
-    return (
-      <Grid container justify="center" alignItems="center" className={classes.loginPage}>
-        <Grid item>
-          <Login />
-        </Grid>
-      </Grid>
-    );
-  }
-  return clientAuthed ? (
-    <Grid>
-      <Navbar />
-      <Component {...pageProps} />
     </Grid>
-  ) : null;
+  );
 });
 
 export default PrimaryLayout;
