aa302e678f961dfc53030b1387161be13465ca4c
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2023-01-13 17:29:15 +0000

    added initialization script

--- .chewy/commands/src/commands/init/index.ts
+++ .chewy/commands/src/commands/init/index.ts
@@ -1,7 +1,6 @@
+import * as chewy from '@gochewy/lib'
 import {Command, Flags} from '@oclif/core'
 import {exec} from 'node:child_process'
-import {cwd} from 'node:process'
-import {promisify} from 'node:util'
 
 export default class InitIndex extends Command {
   static description = 'initializes the component after installation or when getting ready to develop (install dependencies, etc.)'
@@ -20,10 +19,17 @@ export default class InitIndex extends Command {
   static args = [{name: 'file'}]
 
   public async run(): Promise<void> {
-    const execPromise = promisify(exec)
-    console.log('CWD:', cwd())
-    const {stdout, stderr} = await execPromise('yarn install')
-    console.log('stdout:', stdout)
-    console.log('stderr:', stderr)
+    const process = exec('yarn install', {cwd: chewy.files.getComponentDir()})
+    await new Promise((resolve, reject) => {
+      process.stdout?.on('data', data => console.log(data))
+      process.stderr?.on('data', data => console.log(data))
+      process.on('close', code => {
+        if (code === 0) {
+          resolve('Successfully installed dependencies')
+        } else {
+          reject()
+        }
+      })
+    })
   }
 }
