64d7def1fc68d018189efac9986ebc755d607d9d
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2023-01-13 16:36:39 +0000

    start working on init

--- .chewy/commands/src/commands/init/index.ts
+++ .chewy/commands/src/commands/init/index.ts
@@ -1,4 +1,7 @@
 import {Command, Flags} from '@oclif/core'
+import {exec} from 'node:child_process'
+import {cwd} from 'node:process'
+import {promisify} from 'node:util'
 
 export default class InitIndex extends Command {
   static description = 'initializes the component after installation or when getting ready to develop (install dependencies, etc.)'
@@ -17,12 +20,10 @@ export default class InitIndex extends Command {
   static args = [{name: 'file'}]
 
   public async run(): Promise<void> {
-    const {args, flags} = await this.parse(InitIndex)
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /workspace/chewy-global/component-commands/src/commands/init/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    const execPromise = promisify(exec)
+    console.log('CWD:', cwd())
+    const {stdout, stderr} = await execPromise('yarn install')
+    console.log('stdout:', stdout)
+    console.log('stderr:', stderr)
   }
 }
